a9ae6e36c194fb485f41b4a8cf5cb3e532856d4a
sonhaon <97341224+SonHaon@users.noreply.github.com>
2023-11-27 12:30:38 +0100

    finñcours 27/11

--- encodage_image/TP_image_image.py
+++ encodage_image/TP_image_image.py
@@ -1,6 +1,9 @@
 from convert_str_utf8 import str_vers_utf8,utf8_vers_str
 from PIL import Image
 import time
+from pathlib import Path
+
+path = Path(__file__).parent
 
 def split3(message:str):
     message_new=[]
@@ -19,19 +22,58 @@ def split3(message:str):
     return message_new
 
 
-def decodage_message_dans_image(image,image_base):
-    image=Image.open(r"C:\Users\noahv\Desktop\NSI\encodage_image\image_dans_image.png")
+def decodage_image_dans_image():
+    image=Image.open(r"C:\Users\noahv\Desktop\NSI\encodage_image\babouin.png")
+    new_image=image.copy()
     a=0
     for hauteur in range(image.height):
         for longueur in range(image.width):
             r,g,b=image.getpixel((longueur,hauteur))
-            r=bin(r)[:2]
+            r="0"*(8-len(bin(r)[2:]))+bin(r)[2:]
+            g="0"*(8-len(bin(g)[2:]))+bin(g)[2:]
+            b="0"*(8-len(bin(b)[2:]))+bin(b)[2:]
+            r2=int(r[2:],2)
+            g2=int(g[2:],2)
+            b2=int(b[2:],2)
+            new_image.putpixel((longueur,hauteur),(r2,g2,b2))
         a+=1
-
-    message="".join(message).rstrip("0")+"0000000000"
-    # print(message)
-    message=utf8_vers_str(message)
+    try:
+        image.save(r"E:\image_code\\"+"babouin_revele.png")
+        print("save sur clé")
+    except:
+        image.save(path / "babouin_revele.png")
+        print("save sur pc")
 
     image.close()
+    new_image.show()
+    new_image.close()
+
+def encode_image_dans_image(image):
+    image_base=Image.open(path / "babouin.png")
+    image=Image.open(path / image)
+    a=0
+    for hauteur in range(image_base.height):
+        for longueur in range(image_base.width):
+            r,g,b=image_base.getpixel((longueur,hauteur))
+            r2,g2,b2=image.getpixel((longueur,hauteur))
+            r="0"*(8-len(bin(r)[2:]))+bin(r)[2:][::4]
+            g="0"*(8-len(bin(g)[2:]))+bin(g)[2:][::4]
+            b="0"*(8-len(bin(b)[2:]))+bin(b)[2:][::4]
+            r2="0"*(8-len(bin(r2)[2:]))+bin(r2)[2:][::4]
+            g2="0"*(8-len(bin(g2)[2:]))+bin(g2)[2:][::4]
+            b2="0"*(8-len(bin(b2)[2:]))+bin(r2)[2:][::4]
+            image_base.putpixel((longueur,hauteur),(int(r+r2,2),int(g+g2,2),int(b+b2,2)))
+        a+=1
+    try:
+        image_base.save(r"E:\image_code\\"+"babouin_super_secret.png")
+        print("save sur clé")
+    except:
+        image_base.save(path / "babouin_super_secret.png")
+        print("save sur pc")
+
     image_base.close()
-    return message
+    image.show()
+    image.close()
+
+
+decodage_image_dans_image()
