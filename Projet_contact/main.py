58e6bda58219ebc99362e212bb6ff7e6ba42608e
sonhaon macmini <97341224+SonHaon@users.noreply.github.com>
2023-12-11 22:07:26 +0100

    avancer contact

--- Projet_contact/main.py
+++ Projet_contact/main.py
@@ -1,11 +1,19 @@
-import json,os
+import json,os,platform
 from pathlib import Path
 path= Path(__file__).parent
+file_contact=path / "contact.json"
 
-os.system("cls")
+def clear():
+    """
+    Clear the console
+    """
+    command = 'cls' if platform.system().lower().startswith('win') else 'clear'
+    os.system(command)
 
-with os.open((path / "contact.json"),"r") as file:
-    contact=json.load(file)
+
+
+with open((file_contact)) as file:
+    contact:list=json.load(file)
 def ajout_contact():
     global contact
     id=len(contact)
@@ -29,8 +37,48 @@ def ajout_contact():
 
 def supprimmer_contact():
     global contact
+    clear()
+    nom=input("nom : ")
+    if nom=="":nom=None
+    prenom=input("prénom : ")
+    if prenom=="":prenom=None
+    tel=input("numéro de tel : ")
+    if tel=="":tel=None
+    ids=search_contact(prenom,nom,tel)
+    if ids==[]:
+        print('aucun contact trouvé')
+    contact.pop(ids[0])
+
+
+
     
-def voir_contact(id=None,prenom=None,tel=None):
+
+def search_contact(prenom=None,nom=None,tel=None)->list:
+    global contact
+    infos=[]
+    if prenom != None:
+        for id in contact:
+            if contact[id]["prenom"]==prenom:
+                if not id in infos:
+                    infos.append(id)
+    if nom != None:
+        for id in contact:
+            if contact[id]["nom"]==nom:
+                if not id in infos:
+                    infos.append(id)
+    elif tel != None:
+        for id in contact:
+            if contact[id]["tel"]==tel:
+                if not id in infos:
+                    infos.append(id)
+    else:
+        for id in contact:
+            if not id in infos:
+                infos.append(id)
+
+    return infos
+
+def voir_contact(id=None,prenom=None,nom=None,tel=None):
     global contact
     infos=[]
     width=os.get_terminal_size().columns-1
@@ -40,38 +88,24 @@ def voir_contact(id=None,prenom=None,tel=None):
             info.append(f"***** {inf} : {contact[id][inf]}"+" "*(width-len(f"***** {inf} : {contact[id][inf]}")-5)+"*****")
         info="\n".join(info)
         infos.append(info)
-    elif prenom != None:
-        for id in contact:
-            if contact[id]["prenom"]==prenom:
-                info=[f"***** Contact {id} : "+" "*(width-len(f"***** Contact {id} : ")-5)+"*****"]
-                for inf in contact[id]:
-                    info.append(f"***** {inf} : {contact[id][inf]}"+" "*(width-len(f"***** {inf} : {contact[id][inf]}")-5)+"*****")
-                info="\n".join(info)
-                infos.append(info)
-    elif tel != None:
-        for id in contact:
-            if contact[id]["tel"]==tel:
-                info=[f"***** Contact {id} : "+" "*(width-len(f"***** Contact {id} : ")-5)+"*****"]
-                for inf in contact[id]:
-                    info.append(f"***** {inf} : {contact[id][inf]}"+" "*(width-len(f"***** {inf} : {contact[id][inf]}")-5)+"*****")
-                info="\n".join(info)
-                infos.append(info)
     else:
-        for id in contact:
-            info=[f"***** Contact {id} : "+" "*(width-len(f"***** Contact {id} : ")-5)+"*****"]
+        liste=search_contact(prenom,nom,tel)
+        for id in liste:
+            info=[f"*****"+" "*((width-len(f"***** Contact {id} : ")-5)//2)+f"Contact {id} : "+" "*((width-len(f"***** Contact {id} : ")-5)//2)+"*****"]
             for inf in contact[id]:
-                info.append(f"***** {inf} : {contact[id][inf]}"+" "*(width-len(f"***** {inf} : {contact[id][inf]}")-5)+"*****")
+                info.append(f"*****"+" "*((width-len(f"***** {inf} : {contact[id][inf]}")-5)//2)+f"{inf} : {contact[id][inf]}"+" "*((width-len(f"***** {inf} : {contact[id][inf]}")-5)//2)+"*****")
             info="\n".join(info)
             infos.append(info)
     print("*"*width)
-    print("\n**************************************************\n".join(infos))
+    print(("\n"+"*"*width+"\n").join(infos))
     print("*"*width)
     input()
-    os.system("cls")
+    clear()
     
 def menu():
+    global contact
     while True:
-        os.system("cls")
+        clear()
         print("1) ajouter un contact")
         print("2) retirer un contact")
         print("3) modifier un contact")
@@ -79,16 +113,20 @@ def menu():
         print("5) voir les contacts")
         print("99) sortir du programme")
         a=input("choisir une option : ")
-        os.system("cls")
+        clear()
         if a=="1":
             ajout_contact()
+        if a=="2":
+            supprimmer_contact()
         elif a=="4":
             prenom=input("prénom du contact a rechercher : ")
             voir_contact(prenom=prenom)
         elif a=="5":
             voir_contact()
         elif a=="99":
-            os.system("cls")
+            with open(file_contact,"w") as file:
+                file.write(json.dumps(contact))
+            clear()
             break
 
             
