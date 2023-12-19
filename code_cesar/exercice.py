cd60bb400f4b12e65dee6c88737821a814f4c294
sonhaon <97341224+SonHaon@users.noreply.github.com>
2023-12-19 13:43:51 +0100

    fin cours 19/12

--- code_cesar/exercice.py
+++ code_cesar/exercice.py
@@ -1,4 +1,4 @@
-import os,platform,time
+import os,platform,time,math
 def clear():
     """
     Clear the console
@@ -8,7 +8,7 @@ def clear():
 
 clear()
 
-alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ']
+alphabet_simple=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','à','ù','é','è','ê','ï','ô','0','1','2','3','4','5','6','7','8','9','.',',','\'','?','!','&','#','-',' ']
 
 
@@ -39,14 +39,14 @@ def encode_cesar_lettre_alphabet(cLettre: str, iDecalage: int) ->str:
     assert type(cLettre)==str, "la lettre doit etre un string"
     assert len(cLettre)==1, "la lettre doit etre une seul lettre"
     assert type(iDecalage)==int, "le decalage doit etre un entier"
-    if not cLettre.lower() in alphabet:
+    if not cLettre.lower() in alphabet_simple:
         return cLettre
-    lettre = alphabet.index(cLettre.lower())+iDecalage
-    while lettre>(len(alphabet)-1):lettre-len(alphabet)
+    lettre = alphabet_simple.index(cLettre.lower())+iDecalage
+    while lettre>(len(alphabet_simple)-1):lettre-=len(alphabet_simple)
     if cLettre.isupper():
-        return (alphabet[lettre])
+        return (alphabet_simple[lettre])
     else:
-        return (alphabet[lettre]).lower()
+        return (alphabet_simple[lettre]).lower()
 
 def encode_cesar(strMessage: str, iDecalage: int) -> str:
     """
@@ -78,12 +78,37 @@ def code_cesar(strMessage: str, iDecalage: int,encodage:bool):
 
 
 
-# print(encode_cesar_lettre2("A",43))
-# print(encode_cesar_lettre2("b",43))
-# print(encode_cesar_lettre2("C",43))
-# print(encode_cesar_lettre2(" ",43))
-# print(encode_cesar_lettre2("!",43))
+# print(encode_cesar_lettre_alphabet("A",1))
+# print(encode_cesar_lettre_alphabet("b",43))
+# print(encode_cesar_lettre_alphabet("C",43))
+# print(encode_cesar_lettre_alphabet(" ",43))
+# print(encode_cesar_lettre_alphabet("!",43))
 # print(encode_cesar("couCou C'est Moi !",146))
 # print(decode_cesar(encode_cesar("couCou C'est Moi !",146),146))
 
-print(code_cesar("24,0ô'!38584ô,à0&&0-4?24ô'08 ô,4ô2!34ô4 aô-4,0a8c4c4'4?aô 8'&,4ôhô20 4-ôé",33,False))
+# print(code_cesar("24,0ô'!38584ô,à0&&0-4?24ô'08 ô,4ô2!34ô4 aô-4,0a8c4c4'4?aô 8'&,4ôhô20 4-ôé",33,False))
+
+def code_Vigenere(strMessage: str, strCle: str) -> str:
+    while len(strCle)<len(strMessage):
+        strCle+=strCle
+    mess=""
+    for i in range(len(strMessage)):
+        intCle=alphabet_simple.index(strCle[i].lower())
+        mess+=encode_cesar_lettre_alphabet(strMessage[i].lower(),intCle)
+
+    return mess
+
+def decode_Vigenere(strMessage: str, strCle: str) -> str:
+    while len(strCle)<len(strMessage):
+        strCle+=strCle
+    mess=""
+    for i in range(len(strMessage)):
+        intCle=alphabet_simple.index(strCle[i].lower())
+        mess+=encode_cesar_lettre_alphabet(strMessage[i].lower(),-intCle)
+
+    return mess
+
+# print(code_Vigenere("CHIFFRE DE VIGENERE","BACHELIER"))
+# print(decode_Vigenere("dhkmjcm uf xpkpviif","BACHELIER"))
+
+
