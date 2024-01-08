ae6b162739a0e83d04e6a9e0e81fa938f866b2a6
sonhaon <97341224+SonHaon@users.noreply.github.com>
2024-01-08 12:30:58 +0100

    fin 08/01

--- fiche_tableau/exercice.py
+++ fiche_tableau/exercice.py
@@ -49,21 +49,56 @@ def lecture_csv_en_tuples_nommes(nom_du_fichier: str, delimiteur: str)-> list:
     donnée étant stockée sous forme de liste
     """
     with open(path / nom_du_fichier, 'r') as mon_fichier :
-        contenu = csv.reader(mon_fichier, delimiter=';')
+        contenu = csv.reader(mon_fichier, delimiter=delimiteur)
         header=next(contenu)
         createur_de_tuple_nomme = collections.namedtuple(nom_du_fichier.split(".")[0], header)
 
-        elements = list()
+        elements = []
         for ligne in contenu:
             tuple=createur_de_tuple_nomme(*ligne)
             elements.append(tuple)
     # elements est une liste de listes
     for element in elements:
         print(element)
+    return elements
 
+
+def lecture_csv_en_listes2(nom_du_fichier: str, delimiteur: str)->list:
+    """
+    :param delimiteur: type str: le délimiteur : ‘,’ ou ‘;’
+    renvoie une liste contenant toutes les données du fichier csv, chaque
+    donnée étant stockée sous forme de liste
+    """
+    with open(path / nom_du_fichier, 'r') as mon_fichier :
+        contenu = csv.reader(mon_fichier, delimiter=delimiteur)
+        header=next(contenu)
+
+        elements = list()
+        for ligne in contenu:
+            try :
+                # test d'importation
+                assert ligne[1] == 'F' or ligne[1] == 'H'
+                assert int(ligne[2])>0
+                assert float(ligne[3])>0
+                assert len(ligne)==5
+                assert ligne[4]=="oui" or ligne[4]=="non"
+                identifiant = int(ligne[0])
+                elements.append([identifiant, *ligne])
+            except:
+                # En cas d’erreur, le code suivant est exécuté.
+                print("Erreur :", ligne, "ne peut pas être importée")
+    # elements est une liste de listes
+    for element in elements:
+        print(element)
+    mon_fichier.close()
+    # return elements
 
 
 clear()
 # print(lecture_csv_en_listes("fruits.txt",";"))
 # print(lecture_csv_en_dictionnaire("fruits.txt",";"))
-print(lecture_csv_en_tuples_nommes("fruits.txt",";"))
+patients = (lecture_csv_en_tuples_nommes("malaria.csv",","))
+nb=[]
+for patient in patients:
+    if patient.malaria=="positive":nb.append(patient)
+# print("\n".join(nb))
