b53d0d1da42745d76636e3182070f1b8fe8c2371
SonHaon PcIDF <97341224+SonHaon@users.noreply.github.com>
2024-03-12 14:53:36 +0100

    fin cours 12/03

--- web/script_image_zoom.js
+++ web/script_image_zoom.js
@@ -1,16 +1,18 @@
-var modal = document.getElementById('myModal');
-var modal_image = document.getElementById("img_show");
-var images = document.getElementsByClassName('modal-img');
+modal = document.getElementById('myModal');
+modal_image = document.getElementById("img_show");
+liste_images = ['images/I_love_HTML.jpg', 'images/banniere.png', 'images/1200px-HTML5_logo_and_wordmark.svg.png', 'images/NSI.webp', 'images/Bandeau2_NSI.jpg', 'images/logo-st-e.jpg', 'images/i-love-python.png', 'images/CSS3_logo_and_wordmark.svg.png', 'images/html.png', 'images/specilaite-nsi.jpeg'];
 
-for (var i = 0; i < images.length; i++) {
-    images[i].onclick = function(){
-        modal.style.display = "block";
-        modal_image.src = this.src;
-    }
+
+function Unzoom() {
+    modal.style.display = "none"
 }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function Zoom_slideshow(i) {
+    modal.style.display = "block"
+    modal_image.src = liste_images[i-1]
+}
+
+function Zoom(src) {
+    modal.style.display = "block"
+    modal_image.src = src
 }
--- web/script_slideshow_images.js
+++ web/script_slideshow_images.js
@@ -1,5 +1,5 @@
-var slideIndex = 1;
-var liste_images = ['images/I_love_HTML.jpg', 'images/banniere.png', 'images/1200px-HTML5_logo_and_wordmark.svg.png', 'images/NSI.webp', 'images/Bandeau2_NSI.jpg', 'images/logo-st-e.jpg', 'images/i-love-python.png', 'images/CSS3_logo_and_wordmark.svg.png', 'images/html.png', 'images/specilaite-nsi.jpeg'];
+slideIndex = 1;
+liste_images = ['images/I_love_HTML.jpg', 'images/banniere.png', 'images/1200px-HTML5_logo_and_wordmark.svg.png', 'images/NSI.webp', 'images/Bandeau2_NSI.jpg', 'images/logo-st-e.jpg', 'images/i-love-python.png', 'images/CSS3_logo_and_wordmark.svg.png', 'images/html.png', 'images/specilaite-nsi.jpeg'];
 
 showSlides(slideIndex);
 
@@ -8,9 +8,12 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-    var images = liste_images
-    var slides = document.getElementsByClassName("slide");
-    if (n > images.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = images.length}
-    slides[0].src = images[slideIndex-1];
+    slides = document.getElementsByClassName("slider");
+    if (n > liste_images.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = liste_images.length}
+    slides[0].src = liste_images[slideIndex-1];
+}
+
+function index() {
+    return slideIndex
 }
